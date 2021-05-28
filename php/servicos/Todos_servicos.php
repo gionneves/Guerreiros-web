5d846f92e09af22bd446905e669ae7dcd2629ec9
Giovanni <gionneves@gmail.com>
2021-05-28 17:49:34 -0300

    Template de servicos

---
+++ php/servicos/Todos_servicos.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Mostrar todos os serviços cadastrados
+ * 
+ * PHP version 7
+ * 
+ * @category Serviços
+ * @package  Serviços
+ * @author   Giovanni Neves Sadauscas <gionneves@gmail.com>
+ * @license  Guerreiros games
+ * @link     http//localhost/
+ */
+?>
+
+<div class="todos_servicos">
+    <div class="container">
+        <table>
+            <thead>
+                <th scope="col">ID</th>
+                <th scope="col">ID</th>
+                <th scope="col">ID</th>
+                <th scope="col">ID</th>
+                <th scope="col">ID</th>
+            </thead>
+            <tbody>
+                <?php 
+                require '../Conexao.php';
+                sql = "";
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
