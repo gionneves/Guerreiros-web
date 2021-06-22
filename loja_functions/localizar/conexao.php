8615d6b0d3f167fbed329d08d694ee03dfb7e520
Giovanni <gionneves@gmail.com>
2021-06-22 17:19:42 -0300

    Updates conexao

--- loja_functions/localizar/conexao.php
+++ loja_functions/localizar/conexao.php
@@ -1,4 +1,14 @@
-<?php
+<?php/**
+ * Faz a conexão com o banco de dados
+ * 
+ * PHP version 7
+ * 
+ * @category Conexão
+ * @package  Conexão
+ * @author   Giovanni Neves Sadauscas <gionneves@gmail.com>
+ * @license  Guerreiros games
+ * @link     http//localhost/
+ */
 
 $dsn = 'mysql:host=localhost;dbname=dbguerra;charset=utf8';
 $usuario = 'root';
